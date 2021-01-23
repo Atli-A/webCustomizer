9d6c16954aa8f2513a7e7d24a4b3f48cb6fcfede
Atli Arnarsson <atli.nikulas@gmail.com>
2021-01-23 15:01:50 -0800

    added user storage

--- popup.js
+++ popup.js
@@ -5,8 +5,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let scrollbarCustom = true;
 
+    console.log(storageGet)
+
+    if (chrome.storage.sync.get(['darkScroll'], function (result) {}) == undefined) {
+        chrome.storage.sync.set({ darkScroll: false })
+    }
+
+    scrollbarCustom = chrome.storage.sync.get(['darkScroll'], function() {})
+
 
     document.getElementById("changeScrollbar").addEventListener("change", alerter, false)
+
+    function storageGet(name) {
+        return chrome.storage.sync.get(['darkScroll'], function (result) {});
+    }
 
 
 
@@ -15,9 +27,22 @@ document.addEventListener("DOMContentLoaded", function () {
             function (tabs) {
                 // chrome.tabs.sendMessage(tabs[0].id, "hi")
             })
-        if (scrollbarCustom) {
-            chrome.tabs.insertCSS( { file: "content.css" });
+        if (chrome.storage.sync.get(['darkScroll'])) {
+            chrome.tabs.insertCSS({ file: "css/content.css" });
+        }
+        else {
+            chrome.tabs.insertCSS({ file: "css/content.css" });
+
         }
         // alert("yeet")
     }
 }, false)
+
+
+// chrome.storage.sync.set({ key: value }, function () {
+//     console.log('Value is set to ' + value);
+// });
+
+chrome.storage.sync.get(['darkScroll'], function (result) {
+    console.log('Value currently is ' + result.key);
+});
